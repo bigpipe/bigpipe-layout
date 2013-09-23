eee1d8df47ddb1a5049f3e66a6c7fb083fb31819
Swaagie <info@martijnswaagman.nl>
2013-09-24 00:42:55 +0200

    [test] prepare tests

--- test/index.test.js
+++ test/index.test.js
@@ -9,15 +9,32 @@ describe('Bigpipe default page layout plugin', function () {
     , expect = chai.expect
     , server;
 
-    layout.base = __dirname + '/fixtures/base.ejs';
-    beforeEach(function () {
-      server = Pipe.createServer(1337, {
-        public: __dirname,
-        dist: '/tmp/dist'
-      }).use(layout);
-    });
+  layout.options = {
+    base: __dirname + '/fixtures/base.ejs'
+  };
 
-    afterEach(function () {
-      server = null;
-    });
+  beforeEach(function () {
+    server = Pipe.createServer(1337, {
+      public: __dirname,
+      dist: '/tmp/dist'
+    }).use(layout);
+  });
+
+  afterEach(function () {
+    server = null;
+  });
+
+  it('exposes server side functionality', function () {
+    expect(layout).to.have.property('server');
+    expect(layout.server).to.be.a('function');
+  });
+
+  it('exposes default plugin name', function () {
+    expect(layout).to.have.property('name', 'layout');
+  });
+
+  it('will emit an error if no valid layout is provided');
+  it('will insert content of page in layout');
+  it('allows insertion key to be changed');
+  it('shortcircuits compiled#server only once');
 });
